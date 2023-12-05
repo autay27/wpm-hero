54aae03c267aef54e77edea3903d1ccd5aecd2a6
autay27 <au.tay.27@gmail.com>
2023-12-05 17:44:36 +0000

    set cors policy

--- backend/src/main/java/com/example/wpmhero/OutcomeController.java
+++ backend/src/main/java/com/example/wpmhero/OutcomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.ArrayList;
 
+@CrossOrigin
 @RestController
 public class OutcomeController {
 
@@ -25,7 +27,7 @@ public class OutcomeController {
 	}
 
 	@PostMapping("/outcome")
-	public Outcome page(@RequestBody Outcome o) {
+	public Outcome outcome(@RequestBody Outcome o) {
 
 		return repository.save(o);
 
--- todo.md
+++ todo.md
@@ -1,17 +1,16 @@
 Frontend
-- Send the win / loss to the backend
 - Display wins / losses so far
 - Write 1 test
 
 Backend
-- set up communication with frontend
 - Write 1 test
 
 Plans
-- nodejs events
+- react multipage website + event listening for the timer
 - Change character count to word count (needs some thought put in)
     - Final wordcount
     - Words written (how to record)
-    - Words deleted?
 
 - Codemirror to mimic markdown rendering
+- Saving + loading your writing
+- UI mockup
