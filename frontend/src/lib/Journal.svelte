7e7241938fa134160290a83e91497d3f800e0c26
autay27 <au.tay.27@gmail.com>
2024-01-01 23:26:01 +0000

    journal page prints all outcomes so far

--- frontend/src/lib/Journal.svelte
+++ frontend/src/lib/Journal.svelte
@@ -1,1 +1,52 @@
-I will display the recent challenges
+<script lang="ts">
+import { onMount } from 'svelte';
+import type { Challenge, ChallengeAttempt } from './ChallengeUtils';
+
+interface Outcome {
+    outcome : boolean,
+    challenge : Challenge,
+    attempt: ChallengeAttempt
+}
+   
+
+let outcomes: Outcome[] = []
+
+function printOutcome(o: Outcome): string {
+    console.assert(o.challenge.type == 0, "Only doing total wordcount challenges right now - target wc should not be null")
+    let words = o.challenge.finalWords
+    let time = o.challenge.time
+    let outcome = o.outcome ? "Success" : "Failure"
+
+    return `Challenge: Write ${words} words in ${time/1000} seconds. Result: ${outcome}`
+}
+
+onMount(async () =>{
+
+    try {
+        outcomes = await fetch("http://localhost:8080/alloutcomes",
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        ).then(res => res.json())
+        .then(res => {return res as Outcome[]})
+        
+        console.log("success: " + outcomes.length + " entries")
+        console.log(outcomes)
+
+    } catch (error) {
+        console.log("failure: " + error)
+    }
+
+})
+
+</script>
+
+<h1>Challenges completed so far...</h1>
+<ul>
+    {#each outcomes as o}
+    <li>{printOutcome(o)}</li>
+    {/each}
+</ul>
